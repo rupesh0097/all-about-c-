f55acb340286a6534b98b2264d81aad677251adb
RupeshYadav4 <>
2021-06-26 10:28:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// in this example we will add two number
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y;
+	int sum;
+	cout << "Enter the First Number:";
+	cin >> x;
+	cout <<"Enter the second Number:";
+	cin >> y;
+	sum = x+y;
+	cout << "The sum of first and second number is: " <<
+					sum;
+
+}
